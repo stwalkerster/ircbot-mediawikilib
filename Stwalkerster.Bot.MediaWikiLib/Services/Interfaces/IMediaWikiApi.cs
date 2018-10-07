61a56ae89bffb0f900bd42361e58537e38febc67
Simon Walker <github@stwalkerster.co.uk>
2018-10-07 23:58:20 +0100

    interface :|

--- Stwalkerster.Bot.MediaWikiLib/Services/Interfaces/IMediaWikiApi.cs
+++ Stwalkerster.Bot.MediaWikiLib/Services/Interfaces/IMediaWikiApi.cs
@@ -1,5 +1,6 @@
 ﻿namespace Stwalkerster.Bot.MediaWikiLib.Services.Interfaces
 {
+    using System;
     using System.Collections.Generic;
 
     public interface IMediaWikiApi
@@ -11,5 +12,11 @@
         bool WritePage(string pageName, string content, string editSummary, string timestamp, bool bot, bool minor);
         void DeletePage(string pageName, string reason);
         IEnumerable<string> PrefixSearch(string prefix);
+        int GetCategorySize(string categoryName);
+        IEnumerable<string> GetPagesInCategory(string category);
+        string GetArticlePath();
+        string GetMaxLag();
+        DateTime? GetRegistrationDate(string username);
+        int GetEditCount(string username);
     }
 }
