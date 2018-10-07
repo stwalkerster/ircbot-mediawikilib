07ce4859c11c7d464181aee4812d41d2ea590f61
Simon Walker <github@stwalkerster.co.uk>
2018-10-07 16:17:28 +0100

    Fix interface

--- Stwalkerster.Bot.MediaWikiLib/Services/Interfaces/IMediaWikiApi.cs
+++ Stwalkerster.Bot.MediaWikiLib/Services/Interfaces/IMediaWikiApi.cs
@@ -6,5 +6,10 @@
     {
         IEnumerable<string> GetUserGroups(string user);
         bool PageIsInCategory(string page, string category);
+        void Login();
+        string GetPageContent(string pageName, out string timestamp);
+        bool WritePage(string pageName, string content, string editSummary, string timestamp, bool bot, bool minor);
+        void DeletePage(string pageName, string reason);
+        IEnumerable<string> PrefixSearch(string prefix);
     }
 }
